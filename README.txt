66d8132b52a0951caf00ff438165281842283e4d
Sam Edelstein <samedelstein@users.noreply.github.com>
2023-03-25 14:19:31 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+
+To make this work, install requirements, and then insert your OpenAI API key on line 9 in the code
---
+++ ingredients.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import openai
+
+st.header(':knife_fork_plate:  :orange[Substituted Ingredients]')
+
+
+openai.api_key = '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'
+st.sidebar.header(':fork_and_knife: :blue[Add Your Ingredients Here]')
+
+options = st.sidebar.multiselect(
+    'What dietary restrictions do you have?',
+    ['lactose intolerance', 'vegan', 'vegetarian', 'kosher', 'keto', 'low carb', 'pescatarian', 'gluten free'])
+options_string = ', '.join(options) 
+data = st.sidebar.text_area("Paste Your Ingredients")
+button=st.sidebar.button('Generate Substitutions')
+
+if (button==True):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant that converts recipes"},
+            {"role": "user", "content": 'based on the following ingredient list,  please substitute the ingredients below with ' + options_string + ' friendly ingredients. Create an html table with one column as the original ingredient another column with the substitute ingredient, and a final column with the reason this subsitution is best. Please include the amounts and recommend appropriate amounts: '+ data}
+
+        ]
+    )
+    recipe_name = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant that converts recipes"},
+            {"role": "user", "content": 'based on the following ingredient list,  name this dish: '+ data}
+
+        ]
+    )
+    st.write(recipe_name["choices"][0]["message"]["content"])
+
+    st.write(response["choices"][0]["message"]["content"])
